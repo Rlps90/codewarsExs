651ee6fc116e71a7ef0f444f1443c5866a5432b1
Renan Carvalho <rcrvlh90@gmail.com>
2023-03-23 12:23:13 -0300

    Create 6 kyu Break camelCase.js

---
+++ 6 kyu Break camelCase.js
@@ -0,0 +1,14 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// complete the function
+function solution(string) {
+    
+    string = string.replace(/([a-z])([A-Z])/g, "$1 $2")
+  
+    return string.replace(/\s/, " ")
+}
