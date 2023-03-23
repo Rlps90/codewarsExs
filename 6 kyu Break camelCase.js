9dbd091ca15804c8b8a397d0faf251dc88ae7a51
Renan Carvalho <rcrvlh90@gmail.com>
2023-03-23 15:07:16 -0300

    Update 6 kyu Break camelCase.js

--- 6 kyu Break camelCase.js
+++ 6 kyu Break camelCase.js
@@ -7,8 +7,9 @@
 
 // complete the function
 function solution(string) {
-    
+    //separates words where lowercase character is followed by an uppercase character
     string = string.replace(/([a-z])([A-Z])/g, "$1 $2")
   
+    //returns the string separated by space as requested
     return string.replace(/\s/, " ")
 }
